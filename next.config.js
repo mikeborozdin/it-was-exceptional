12753f1ee115bc87526d3132f18e0de8ee133ffc
Mike Borozdin <mike.borozdin@gmail.com>
2023-10-27 09:35:41 +0100

    100 mb file upload

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
-    serverActionsBodySizeLimit: '30mb',
+    serverActionsBodySizeLimit: '100mb',
   },
   images: {
     domains: [
